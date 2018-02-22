d131e33b7348e0cce199137e8c60f9f23314fff3
jhuashao <joshua.h.shao@gmail.com>
2018-02-22 02:41:43 -0500

    36 mm modal edits

--- scripts.js
+++ scripts.js
@@ -7,3 +7,20 @@ $('.carousel.carousel-slider').carousel({
 });
 
 $('.carousel').carousel();
+
+
+$('.modal').modal({
+    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+    opacity: .5, // Opacity of modal background
+    inDuration: 500, // Transition in duration
+    outDuration: 500, // Transition out duration
+    startingTop: '4%', // Starting top style attribute
+    endingTop: '10%', // Ending top style attribute
+    ready: function (modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+        
+        console.log(modal, trigger);
+    },
+    complete: function () {
+        
+    } // Callback for Modal close
+});
